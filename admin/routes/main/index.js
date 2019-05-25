1d7a31b7d754f534a25275ce2ae87b9f09bb20b7
Demon-Shen <119422342@qq.com>
2019-05-25 13:52:10 +0800

    commit

--- admin/routes/main/index.js
+++ admin/routes/main/index.js
@@ -6,14 +6,49 @@ module.exports = [
         type: 'get',
         method: async(ctx,next)=>{
             console.log('aaa')
-            await ctx.render('main/order_out',{})
+            await ctx.render('main/order_out',{
+                orderList : [{
+                    name:'asd',
+                    time: '2019',
+                    money : '1000'
+                },{
+                    name:'asd',
+                    time: '2019',
+                    money : '1123'
+                }]
+            })
         },
     },{
         name: '/main/orderInto',
         type: 'get',
         method: async(ctx,next)=>{
             console.log('aa')
-            await ctx.render('main/order_in',{})
+            await ctx.render('main/order_in',{
+                head:{
+                    name:'名称',
+                    time:'时间',
+                    price:'价格'
+                },
+                orderList : [{
+                    
+                    name:'asd',
+                
+                    time:'2019',
+                
+                    price:'123123'
+                    
+                },
+                {
+                    
+                    name:'asd',
+                
+                    time:'2019',
+                
+                    price:'123123'
+                    
+                },
+                ]
+            })
         },
     }
 ]
