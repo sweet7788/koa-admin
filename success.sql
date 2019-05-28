04398d02bc6965e1190b53eea7e2893d9053d9c6
Demon-Shen <119422342@qq.com>
2019-05-28 17:25:02 +0800

    commit

--- success.sql
+++ success.sql
@@ -33,7 +33,7 @@ CREATE TABLE `orders` (
   `profit` decimal(8,2) DEFAULT NULL,
   `remake` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -56,7 +56,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `userName` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
